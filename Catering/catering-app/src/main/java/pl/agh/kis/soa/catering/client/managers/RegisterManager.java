49bf507b515cb371c212eed7fddcfdede380c951
Bartosz Gaj <bartosz.gaj4@gmail.com>
2019-06-08 18:13:09 +0200

    Password MD5

--- Catering/catering-app/src/main/java/pl/agh/kis/soa/catering/client/managers/RegisterManager.java
+++ Catering/catering-app/src/main/java/pl/agh/kis/soa/catering/client/managers/RegisterManager.java
@@ -1,5 +1,6 @@
 package pl.agh.kis.soa.catering.client.managers;
 
+import org.apache.commons.codec.binary.Base64;
 import pl.agh.kis.soa.catering.client.services.ClientService;
 import pl.agh.kis.soa.catering.client.services.OrderService;
 import pl.agh.kis.soa.catering.server.model.Client;
@@ -10,6 +11,8 @@ import pl.agh.kis.soa.catering.server.model.UserRole;
 import javax.annotation.PostConstruct;
 import javax.faces.bean.*;
 import javax.xml.registry.infomodel.User;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 
 @ManagedBean(name = "RegisterManager")
@@ -37,6 +40,23 @@ public class RegisterManager {
     }
 
     public String registerUser(){
+
+        String password = userAccountL.getPassword();
+        MessageDigest md = null;
+        try {
+            md = MessageDigest.getInstance("SHA");
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        byte[] passwordBytes = password.getBytes();
+        byte[] hash = md.digest(passwordBytes);
+        String base64String = Base64.encodeBase64String(hash);
+        System.out.println("skrót hasła: " + base64String);
+
+
+
+
+        userAccountL.setPassword(base64String);
         UserRole userRole = clientService.getUserRole("Manager");
         userAccountL.setUserRole(userRole);
         client.setUserAccount(userAccountL);
