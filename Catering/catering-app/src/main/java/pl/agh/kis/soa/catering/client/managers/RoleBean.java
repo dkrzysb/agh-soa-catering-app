2beaf92de2522df1da657de4e550cf2c9074bead
Bartosz Gaj <bartosz.gaj4@gmail.com>
2019-06-11 04:34:23 +0200

    Index navbar based on role

---
+++ Catering/catering-app/src/main/java/pl/agh/kis/soa/catering/client/managers/RoleBean.java
@@ -0,0 +1,30 @@
+package pl.agh.kis.soa.catering.client.managers;
+
+import javax.faces.bean.ApplicationScoped;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
+import javax.servlet.http.HttpServletRequest;
+
+@ManagedBean(name = "RoleBean")
+@SessionScoped
+public class RoleBean {
+
+    public boolean isUserAdmin() {
+        HttpServletRequest origRequest = (HttpServletRequest) FacesContext.getCurrentInstance().getExternalContext().getRequest();
+        return origRequest.isUserInRole("Admin");
+    }
+
+
+    public boolean isUserManager() {
+        HttpServletRequest origRequest = (HttpServletRequest) FacesContext.getCurrentInstance().getExternalContext().getRequest();
+        return (origRequest.isUserInRole("Manager") || origRequest.isUserInRole("Admin"));
+    }
+
+
+    public boolean isUserStaff() {
+        HttpServletRequest origRequest = (HttpServletRequest) FacesContext.getCurrentInstance().getExternalContext().getRequest();
+        return (origRequest.isUserInRole("Staff") || origRequest.isUserInRole("Admin"));
+    }
+
+}
